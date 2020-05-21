aeef80a7ba93e8d0c59a43007355f4b1b399d643
Vladi Benesova <vladi.benesova1@gmail.com>
2020-05-21 13:39:57 +0200

    testovani a oprava chyb v kodu

--- index.js
+++ index.js
@@ -1,26 +1,30 @@
 'use strict';
 
 let osoba1 = {
-  jmeno: 'Alena',
+  name: 'Alena',
   uspory: 53000
 };
 let osoba2 = {
-  jmeno: 'Karolína',
+  name: 'Karolína',
   uspory: 68000
 };
-if (osoba1.uspory + osoba2.uspory === 100000) {
-  alert('Lets go!');
-  if (osoba1.uspory >= 50000 && osoba2.uspory >= 50000) {
-    console.log('You are a great team!');
-  } else if (osoba1.uspory < 50000) {
-    console.log(`${osoba1.name} has to pay ${50000 - osoba1.uspory}`);
+function checkIt() {
+  if ((osoba1.uspory + osoba2.uspory) >= 100000) {
+    alert('Lets go!');
+    if (osoba1.uspory >= 50000 && osoba2.uspory >= 50000) {
+      console.log('You are a great team!');
+    } else if (osoba1.uspory < 50000) {
+      console.log(`${osoba1.name} has to pay ${50000 - osoba1.uspory}`);
+    } else {
+      console.log(`${osoba2.name} has to pay ${50000 - osoba2.uspory}`);
+    }
   } else {
-    console.log(`${osoba2.name} has to pay ${50000 - osoba2.uspory}`);
-  }
-} else {
-  alert(`Try to save more! You need extra ${100000 - (osoba1.uspory + osoba2.uspory)}`);
-  if (osoba1.uspory < 50000) { console.log(`${osoba1.name} has to save extra ${50000 - osoba1.uspory}`); } else {
-    console.log(`${osoba2.name} has to save extra ${50000 - osoba2.uspory}`);
+    alert(`Try to save more! You need extra ${100000 - (osoba1.uspory + osoba2.uspory)}`);
+    if (osoba1.uspory < 50000) {
+      console.log(`${osoba1.name} has to save extra ${50000 - osoba1.uspory}`);
+    } else {
+      console.log(`${osoba2.name} has to save extra ${50000 - osoba2.uspory}`);
+    }
   }
 }
 
