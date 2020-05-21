d2a9090effa1883bb2a81b41b6de0efdd013177b
Vladi Benesova <vladi.benesova1@gmail.com>
2020-05-21 15:49:30 +0200

    add html and css

--- index.js
+++ index.js
@@ -1,29 +1,35 @@
-'use strict';
+const Person = {
+  constructor: function (name, uspory) {
+    this.name = 'name';
+    this.uspory = 'uspory';
+  }
+};
+//const name1 = document.querySelector('.name1');
 
-let osoba1 = {
-  name: 'Alena',
-  uspory: 53000
-};
-let osoba2 = {
-  name: 'Karolína',
-  uspory: 68000
-};
+const osoba1 = Object.create(Person);
+osoba1.constructor('Alena', 53000);
+
+const osoba2 = Object.create(Person);
+osoba2.constructor('Karolina', 48000);
+
 function checkIt() {
   if ((osoba1.uspory + osoba2.uspory) >= 100000) {
     alert('Lets go!');
     if (osoba1.uspory >= 50000 && osoba2.uspory >= 50000) {
-      console.log('You are a great team!');
+      return 'You are a great team!';
     } else if (osoba1.uspory < 50000) {
-      console.log(`${osoba1.name} has to pay ${50000 - osoba1.uspory}`);
+      return `${osoba1.name} has to pay ${50000 - osoba1.uspory}`;
     } else {
-      console.log(`${osoba2.name} has to pay ${50000 - osoba2.uspory}`);
+      return `${osoba2.name} has to pay ${50000 - osoba2.uspory}`;
     }
   } else {
     alert(`Try to save more! You need extra ${100000 - (osoba1.uspory + osoba2.uspory)}`);
-    if (osoba1.uspory < 50000) {
-      console.log(`${osoba1.name} has to save extra ${50000 - osoba1.uspory}`);
+    if (osoba1.uspory < 50000 && osoba2.uspory < 50000) {
+      return `${osoba1.name} has to save extra ${50000 - osoba1.uspory} and ${osoba2.name} has to save extra ${50000 - osoba2.uspory}`;
+    } else if (osoba1.uspory < 50000) {
+      return `${osoba1.name} has to save extra ${50000 - osoba1.uspory}`;
     } else {
-      console.log(`${osoba2.name} has to save extra ${50000 - osoba2.uspory}`);
+      return `${osoba2.name} has to save extra ${50000 - osoba2.uspory}`;
     }
   }
 }
