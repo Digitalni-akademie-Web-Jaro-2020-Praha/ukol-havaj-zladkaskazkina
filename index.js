7ab6908c163b1f2a091c173c6adf00bfe75ec418
Vladi Benesova <vladi.benesova1@gmail.com>
2020-05-21 16:05:41 +0200

    debbuging

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const Person = {
   constructor: function (name, uspory) {
-    this.name = 'name';
-    this.uspory = 'uspory';
+    this.name = name;
+    this.uspory = uspory;
   }
 };
-//const name1 = document.querySelector('.name1');
+const name1 = document.querySelector('.name1');
+const name2 = document.querySelector('.name2');
+const savings1 = document.querySelector('.save1');
+const savings2 = document.querySelector('.save2');
+const cost = document.querySelector('.cost');
+const checkBtn = document.querySelector('.check-btn');
 
 const osoba1 = Object.create(Person);
-osoba1.constructor('Alena', 53000);
+osoba1.constructor('Alena', 43000);
 
 const osoba2 = Object.create(Person);
 osoba2.constructor('Karolina', 48000);
@@ -25,7 +30,8 @@ function checkIt() {
   } else {
     alert(`Try to save more! You need extra ${100000 - (osoba1.uspory + osoba2.uspory)}`);
     if (osoba1.uspory < 50000 && osoba2.uspory < 50000) {
-      return `${osoba1.name} has to save extra ${50000 - osoba1.uspory} and ${osoba2.name} has to save extra ${50000 - osoba2.uspory}`;
+      return `${osoba1.name} has to save extra ${50000 - osoba1.uspory} and 
+              ${osoba2.name} has to save extra ${50000 - osoba2.uspory}`;
     } else if (osoba1.uspory < 50000) {
       return `${osoba1.name} has to save extra ${50000 - osoba1.uspory}`;
     } else {
@@ -34,3 +40,4 @@ function checkIt() {
   }
 }
 
+checkBtn.addEventListener("submit", checkIt);
