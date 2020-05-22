07cbd349c331abe8bd141519a7c6f55504d19cd4
Vladi Benesova <vladi.benesova1@gmail.com>
2020-05-22 23:30:00 +0200

    add form and eventlistener

--- index.js
+++ index.js
@@ -1,43 +1,50 @@
-const Person = {
-  constructor: function (name, uspory) {
-    this.name = name;
-    this.uspory = uspory;
-  }
-};
-const name1 = document.querySelector('.name1');
-const name2 = document.querySelector('.name2');
-const savings1 = document.querySelector('.save1');
-const savings2 = document.querySelector('.save2');
-const cost = document.querySelector('.cost');
-const checkBtn = document.querySelector('.check-btn');
+function Person(name, uspory) {
+  this.name = name;
+  this.uspory = uspory;
+}
+const osoba1 = new Person('Anna', 30000);
+const osoba2 = new Person('Kate', 37000);
 
-const osoba1 = Object.create(Person);
-osoba1.constructor('Alena', 43000);
+const name1 = document.querySelector('.name1'),
+  name2 = document.querySelector('.name2'),
+  savings1 = document.querySelector('.save1'),
+  savings2 = document.querySelector('.save2'),
+  cost = document.querySelector('.cost'),
+  checkBtn = document.querySelector('.check-btn');
 
-const osoba2 = Object.create(Person);
-osoba2.constructor('Karolina', 48000);
+
+let holidayCost = 0;
+let halfCost = 0;
 
 function checkIt() {
-  if ((osoba1.uspory + osoba2.uspory) >= 100000) {
-    alert('Lets go!');
-    if (osoba1.uspory >= 50000 && osoba2.uspory >= 50000) {
-      return 'You are a great team!';
-    } else if (osoba1.uspory < 50000) {
-      return `${osoba1.name} has to pay ${50000 - osoba1.uspory}`;
+  event.preventDefault();
+  osoba1.name = name1.value;
+  osoba2.name = name2.value;
+  osoba1.uspory = +savings1.value;
+  osoba2.uspory = +savings2.value;
+
+  holidayCost = +cost.value;
+  halfCost = holidayCost / 2;
+
+  if ((osoba1.uspory + osoba2.uspory) >= holidayCost) {
+    if ((osoba1.uspory >= halfCost) && (osoba2.uspory >= halfCost)) {
+      return `You save enough to have a great holiday! You have ${osoba1.uspory + osoba2.uspory}`;
+    } else if (osoba1.uspory < halfCost) {
+      return `You could go, but ${osoba1.name} has to pay ${halfCost - osoba1.uspory}`;
     } else {
-      return `${osoba2.name} has to pay ${50000 - osoba2.uspory}`;
+      return `You could go, but ${osoba2.name} has to pay ${halfCost - osoba2.uspory}`;
     }
   } else {
-    alert(`Try to save more! You need extra ${100000 - (osoba1.uspory + osoba2.uspory)}`);
-    if (osoba1.uspory < 50000 && osoba2.uspory < 50000) {
-      return `${osoba1.name} has to save extra ${50000 - osoba1.uspory} and 
-              ${osoba2.name} has to save extra ${50000 - osoba2.uspory}`;
-    } else if (osoba1.uspory < 50000) {
-      return `${osoba1.name} has to save extra ${50000 - osoba1.uspory}`;
+    if ((osoba1.uspory < halfCost) && (osoba2.uspory < halfCost)) {
+      return `Try to save extra ${holidayCost - (osoba1.uspory + osoba2.uspory)}! 
+      ${osoba1.name} has to save extra ${halfCost - osoba1.uspory} and 
+              ${osoba2.name} has to save extra ${halfCost - osoba2.uspory}`;
+    } else if (osoba1.uspory < halfCost) {
+      return `${osoba1.name} has to save extra ${halfCost - osoba1.uspory}`;
     } else {
-      return `${osoba2.name} has to save extra ${50000 - osoba2.uspory}`;
+      return `${osoba2.name} has to save extra ${halfCost - osoba2.uspory}`;
     }
   }
 }
 
-checkBtn.addEventListener("submit", checkIt);
+checkBtn.addEventListener('click', checkIt);
